67d2bb6415428a03e037c8c4d2af9a4e3d296949
Victor Hugo <victor_hugo1024@hotmail.com>
2016-03-08 17:45:12 -0600

    Corrected validation on email user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,6 @@ class UsersController < ApplicationController
 end
   private
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:username, :email, :email_confirmation, :password)
   end
 end
