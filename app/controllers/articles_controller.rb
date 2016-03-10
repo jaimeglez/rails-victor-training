c5b63143d0307996cd5f6b0c93b23d3707c1d7b7
Victor Hugo <victor_hugo1024@hotmail.com>
2016-03-10 13:36:50 -0600

    Fixed edit, show and index of relation user with articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,31 +9,32 @@ class ArticlesController < ApplicationController
   end
 
   def create
-    #@article = Article.new(article_params)
     @user = User.find(params[:user_id])
     @article = @user.articles.create(article_params)
 
     if @article.save
-      redirect_to user_article_url(@user)
+      redirect_to user_articles_path(@user)
     else
       render 'new'
     end
   end
 
   def show
+    @user=User.find(params[:user_id])
     @article = Article.find(params[:id])
   end
 
  def edit
+   @user=User.find(params[:user_id])
    @article = Article.find(params[:id])
  end
 
  def update
-   #@article = Article.find(params[:id])
    @user = User.find(params[:user_id])
-   @article=@user.articles.find(:id)
-   if @article.update(article_params)
-     redirect_to @article
+   @article=Article.find(params[:id])
+
+   if @user.articles.update( @article, article_params)
+     redirect_to user_articles_path(@user)
    else
      render 'edit'
    end
