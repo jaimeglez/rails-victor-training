6fcfd119a81193f4a7a1dc55c5f0e66bc4a5d001
Victor Hugo <victor_hugo1024@hotmail.com>
2016-03-28 11:42:35 -0600

    Added method destroy on users and articles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :articles
+  has_many :articles, dependent: :destroy
   validates :username, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }, length: { minimum: 8 }, presence: true
   validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, message: " email incorrect" }, presence: true , confirmation: true
   validates :email_confirmation, presence: true
