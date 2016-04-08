7f801bfb10befb9d90253eff3883d1d802a37d86
Victor Hugo <victor_hugo1024@hotmail.com>
2016-04-08 16:34:25 -0500

    Refactored on pull request #10

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,8 @@
 class ArticlesController < ApplicationController
-  #before_filter :find_user, except: [:new]
 
   def index
     @articles = Article.all
   end
-
 
   def show
     @article = Article.find(params[:id])
